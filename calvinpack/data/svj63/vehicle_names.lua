75bacf36acd05d11bea35c04645689380e5ef386
Calvin <vinchvinch1@gmail.com>
2020-07-24 18:49:27 +0700

    nambahinmobil

---
+++ calvinpack/data/svj63/vehicle_names.lua
@@ -0,0 +1,54 @@
+Citizen.CreateThread(function()
+	-- Lamborghini Aventador SVJ
+AddTextEntry('0x0A117B46', 'Lamborghini')
+
+AddTextEntry('0x5D9F4FE9', 'Front and rear bumper plates of Guangdong and Hong Kong')
+
+AddTextEntry('0x6D59A649', 'Only the rear bumper plates of Guangdong and Macao')
+
+AddTextEntry('0x7CB30E10', 'Only the European license plate of the rear bumper')
+
+AddTextEntry('0x9D1505AF', 'Default license plate before and after')
+
+AddTextEntry('0x21A08DEE', 'Remove the 63rd anniversary logo from the door')
+
+AddTextEntry('0x42A16498', 'SVJ roof')
+
+AddTextEntry('0x97CAFB2B', 'Remove front and rear bumper plates')
+
+AddTextEntry('0x117CF230', 'Remove the hood')
+
+AddTextEntry('0x683E6527', 'Only rear bumper license plate in Macao  China')
+
+AddTextEntry('0x1300D476', 'SVJ front bumper')
+
+AddTextEntry('0x2130D70D', 'Only rear bumper Chinese blue license plate')
+
+AddTextEntry('0x39353DFD', 'Front and rear bumper plates of Guangdong and Macao')
+
+AddTextEntry('0x89715E78', 'Only the rear bumper plates of Guangdong and Hong Kong')
+
+AddTextEntry('0x390386B2', 'Front and rear bumper license plate in Macao  China')
+
+AddTextEntry('0x8779239C', 'Front and rear bumper European license plate')
+
+AddTextEntry('0xB40E2286', 'Remove the SVJ logo from the rear fender')
+
+AddTextEntry('0xC2A77E79', 'SVJ skirts')
+
+AddTextEntry('0xC3DE9C66', 'Only rear bumper license plate in Hong Kong  China')
+
+AddTextEntry('0xCEA0B1EA', 'Front and rear bumper license plate in Hong Kong  China')
+
+AddTextEntry('0xD7BB47C6', 'Aventador SVJ 63 Coupe')
+
+AddTextEntry('0xDAACB03C', 'Remove the 63rd anniversary logo from the Trunk lid')
+
+AddTextEntry('0xE83F9FB6', 'SVJ hood')
+
+AddTextEntry('0xE2224D21', 'SVJ rear bumper')
+
+AddTextEntry('0xF9F1888F', 'Front and rear bumper Chinese blue license plate')
+
+
+end)
